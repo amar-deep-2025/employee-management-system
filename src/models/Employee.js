9c25d45d37f9a463e860d09368a75c3768972fb7
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-23 13:27:07 +0530

    add employee.js means model

---
+++ src/models/Employee.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const employeeSchema=new mongoose.Schema(
+  {
+    name:{type:String, required:true},
+    email:{type:String, required:true, unique:true},
+    department:{type:String, required:true},
+    destination:{type:String, required:true},
+},
+ {timestamps:true}
+
+);
+
+module.exports=mongoose.model("Employee",employeeSchema);

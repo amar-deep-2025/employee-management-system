6ee1f6c911c24d53c4bf0c17edb11997f74e186c
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-23 15:40:58 +0530

    refcator Employee.js

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
-
-const employeeSchema=new mongoose.Schema(
+const employeeSchema = new mongoose.Schema(
   {
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    department:{type:String, required:true},
-    destination:{type:String, required:true},
-},
- {timestamps:true}
-
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    department: { type: String, required: true },
+    designation: { type: String, required: true },
+  },
+  { timestamps: true }
 );
 
-module.exports=mongoose.model("Employee",employeeSchema);
+module.exports = mongoose.model("Employee", employeeSchema);

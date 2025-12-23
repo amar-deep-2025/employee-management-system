399bb214f43afce07c86b3d7d21388e4d0d002b1
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-23 13:50:49 +0530

    add routes.js

---
+++ src/routes/Employee/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/Employee/controller");
+
+router.post("/", controller.createEmployee);
+
+router.get("/", controller.getEmployees);
+
+router.get("/:id", controller.getEmployeeById);
+
+router.put("/:id", controller.updateEmployee);
+
+router.delete("/:id", controller.deleteEmployee);
+
+module.exports = router;

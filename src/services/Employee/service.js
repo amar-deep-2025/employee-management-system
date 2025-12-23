b953a0b6074752fe052e1d9d8be0299ece02e5e3
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-23 13:44:03 +0530

    add service.js

---
+++ src/services/Employee/service.js
@@ -0,0 +1,36 @@
+const Employee = require("../models/Employee");
+
+//create
+
+const createEmployee = async (data) => {
+  return await Employee.create(data);
+};
+
+//Retrieve
+
+const getEmployees = async () => {
+  return await Employee.find();
+};
+
+//By id
+const getEmployeeById = async (id) => {
+  return await Employee.findById(id);
+};
+
+//Update
+const updateEmployee = async (id, data) => {
+  return await Employee.findByIdAndUpdate(id, data, { new: true });
+};
+
+//delete
+const deleteEmployee = async (id) => {
+  return await Employee.findBuAndDelete(id);
+};
+
+module.exports = {
+  createEmployee,
+  getEmployees,
+  getEmployeeById,
+  updateEmployee,
+  deleteEmployee,
+};

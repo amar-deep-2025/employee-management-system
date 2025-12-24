6ea1dd37be787fb5959931ae442b397bd2ee865e
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-24 11:30:50 +0530

    improve-1 add middleware errorHandler in app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const employeeRoute = require("./routes/employee.routes");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/employees", employeeRoute);
+
+app.use(errorHandler);
 
 module.exports = app;

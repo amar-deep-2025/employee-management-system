72e91ad8f5d378a4745cfaac46f9a1de231d589c
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-24 11:11:20 +0530

    refactor with validators

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -2,13 +2,27 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/employee.controller");
 
-router.post("/", controller.createEmployee);
+const validate = require("./middlewares/validate");
+
+const validator = require("../validators/employee.validator");
+
+router.post(
+  "/",
+  validator.createEmployeeValidator,
+  validate,
+  controller.createEmployee
+);
 
 router.get("/", controller.getEmployees);
 
 router.get("/:id", controller.getEmployeeById);
 
-router.put("/:id", controller.updateEmployee);
+router.put(
+  "/:id",
+  validator.updateEmployeeValidator,
+  validate,
+  controller.updateEmployee
+);
 
 router.delete("/:id", controller.deleteEmployee);
 

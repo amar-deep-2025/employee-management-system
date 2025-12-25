55cbe625d636d198147bf22922626e028b332be5
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-25 12:43:05 +0530

    add token and generateToken

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-exports.generationToken = (userId) => {
-  return jwt.sign({ id: userid }, process.env.JWT_SECRET, { expiresIn: "1d" });
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
+
+module.exports = { generateToken };
